6ddbea4c7dff168e0b434b2d760b73a135fc0cc1
thexthesecond <bernardothesecond@gmail.com>
2024-12-19 13:48:46 -0300

    updated code

--- SerialBT_Test.ino
+++ SerialBT_Test.ino
@@ -1,5 +1,5 @@
-#include "BluetoothSerial.h"
 #include <TomIBT2.h>
+#include "BluetoothSerial.h"
 
 #define LMOTOR_PIN_R_EN 4
 #define LMOTOR_PIN_L_EN 5
@@ -16,13 +16,15 @@ TomIBT2 motorR(RMOTOR_PIN_R_EN, RMOTOR_PIN_L_EN, RMOTOR_PIN_RPWM, RMOTOR_PIN_LPW
 
 String device_name = "ESP32-BT-Slave";
 
+// Check if Bluetooth is available
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+// Check Serial Port Profile
 #if !defined(CONFIG_BT_SPP_ENABLED)
 #error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
-#endif   // PWM 490.20Hz
+#endif
 
 BluetoothSerial SerialBT;
 
@@ -31,71 +33,49 @@ void setup() {
   SerialBT.begin(device_name);
   //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
   Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-
-  motorL.begin();
-  motorR.begin();
 }
 
 void loop() {
-
-  String input;
-
-  if (!SerialBT.hasClient()) {
-    ForceStop(); // Metódo que para os motores se a conexão for perdida com o CLIENT.
-  }
-
-  if (SerialBT.available()) {
-    input = SerialBT.readString();
-    input.trim();
-
-    robotIO(input);
-  }
+  bool strainControl;
+  char input;
   
+  if (SerialBT.available()) {
+    input = SerialBT.read();
+
+    RobotIO(input, strainControl);
+  }
+
   delay(20);
 }
 
-// Método que manda mensagem tanto para o Serial quanto para o Terminal
-void GlobalPrint(String line) {
-  SerialBT.println(line);
-  Serial.println(line);
-}
-
-void ForceStop() {
-  GlobalPrint("Client unavailable, motors stopped.");
-  motorL.brake();
-  motorR.brake();
-}
-
-//Input/Output do Serial
-void robotIO(String input) {
-  
-  if(input.equals("F")) {
-    GlobalPrint("FORWARD");
-    motorL.rotate(255, TomIBT2::CW);
-    motorR.rotate(255, TomIBT2::CCW);
+void RobotIO(char input, bool strain) {
+  if (input == 'F') {
+    Serial.println("FORWARD");
   }
 
-  if(input.equals("B")) {
-    GlobalPrint("BACKWARDS");
-    motorL.rotate(255, TomIBT2::CCW);
-    motorR.rotate(255, TomIBT2::CW);
+  if (input == 'R') {
+    Serial.println("RIGHT");
   }
 
-  if(input.equals("L")) {
-    GlobalPrint("LEFT");
-    motorL.rotate(255, TomIBT2::CCW);
-    motorR.rotate(255, TomIBT2::CCW);
+  if (input == 'L') {
+    Serial.println("LEFT");
   }
 
-  if(input.equals("R")) {
-    GlobalPrint("RIGHT");
-    motorL.rotate(255, TomIBT2::CW);
-    motorR.rotate(255, TomIBT2::CW);
+  if (input == 'B') {
+    Serial.println("BACK");
   }
 
-  if(input.equals("P")) {
-    GlobalPrint("STOPPED");
-    motorL.rampDown(2000);
-    motorR.rampDown(2000);
+  if (input == 'P') {
+    Serial.println("STOPPED");
+  }
+
+  if (input == 'O') {
+    Serial.println("STRAIN ON");
+    strain = true;
+  }
+
+  if (input == 'X') {
+    Serial.println("STRAIN OFF");
+    strain = false;
   }
 }
