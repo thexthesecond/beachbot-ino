486debfa2c9c5d47e747669da13caa588efce986
thexthesecond <bernardothesecond@gmail.com>
2025-01-08 13:26:23 -0300

    edited code

--- 9163BeachBOT.ino
+++ 9163BeachBOT.ino
@@ -13,12 +13,10 @@
 
 String device_name = "ESP32-BT-Slave";
 
-// Check if Bluetooth is available
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-// Check Serial Port Profile
 #if !defined(CONFIG_BT_SPP_ENABLED)
 #error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
 #endif
@@ -30,7 +28,7 @@ TomIBT2 R_MOTOR(RMOTOR_PIN_R_EN, RMOTOR_PIN_L_EN, RMOTOR_PIN_RPWM, RMOTOR_PIN_LP
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin(device_name);  //Bluetooth device name
+  SerialBT.begin(device_name);
   //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
   Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
 
